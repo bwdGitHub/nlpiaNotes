95c489a22483940fdbaa0ccee766b28ed9b8e1fc
Ben Wells-Day <benwellsday@gmail.com>
2020-02-09 17:13:56 +0000

    wrappers for nltk tokenizers

--- chapter2/tokenizers.py
+++ chapter2/tokenizers.py
@@ -1,4 +1,6 @@
 import re
+from nltk.tokenize import RegexpTokenizer
+from nltk.tokenize import TreebankWordTokenizer
 
 def naiveTokenize(s):
     # naive tokenizer
@@ -9,3 +11,16 @@ def regexTokenize(s):
     # >>> pattern = re.compile(someRegex)
     # >>> pattern.split(stuff)
     return re.split(r'[-\s.,;!?]+', s)
+
+def nltkRegexTokenize(s):
+    # wrapper for the nltk regex tokenizer using the book example
+    t = RegexpTokenizer(r'\w+|$[0-9.]+|\S+')
+    return t.tokenize(s)
+
+def treebankTokenize(s):
+    # wrapper for the nltk treebank tokenizer
+    t = TreebankWordTokenizer()
+    return t.tokenize(s)
+
+
+# there is also nltk.tokenize.casual - intended for text data from something like twitter.
