2fdf0e64f6f738600963207b518c86dab291a957
Ben Wells-Day <benwellsday@gmail.com>
2020-02-09 02:32:24 +0000

    comment

--- chapter2/tokenizers.py
+++ chapter2/tokenizers.py
@@ -5,4 +5,7 @@ def naiveTokenize(s):
     return str.split(s)
 
 def regexTokenize(s):
+    # in practice you may want to compile the regex, then call that compiled pattern, i.e.
+    # >>> pattern = re.compile(someRegex)
+    # >>> pattern.split(stuff)
     return re.split(r'[-\s.,;!?]+', s)
