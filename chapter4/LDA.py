8045497e562a49c92cdb4f4f8b8b97f68f820cc1
Ben Wells-Day <benwellsday@gmail.com>
2020-04-06 00:01:34 +0100

    implement LDA type model

--- chapter4/LDA.py
+++ chapter4/LDA.py
@@ -2,6 +2,8 @@
 # Perform binary classification by finding the line between the centroids of the two classes
 import pandas as pd
 from nlpia.data.loaders import get_data
+from sklearn.feature_extraction.text import TfidfVectorizer
+from nltk.tokenize.casual import casual_tokenize
 
 def get_sms_data():
     # the data is 4837 sms messages of which 638 are spam
@@ -15,5 +17,42 @@ def preprocess():
     sms['spam'] = sms.spam.astype(int)
     return sms
 
-    
+def encode(data):
+    mdl = TfidfVectorizer(tokenizer = casual_tokenize)
+    return mdl.fit_transform(raw_documents=data).toarray()
 
+# Note - the vocab of 'sms-spam' is 9232 words, with 638 spam messages.
+# # A model like Naive Bayes may not do well here because the vocab is to high whilst number of examples with the spam label is propotionally low.
+
+def get_centroid(data):
+    # return the centroid of data
+    return data.mean(axis=0)
+
+def get_masked_centroids(data,mask):
+    # return two centroids, one where mask is true and one where mask is false
+    return [get_centroid(data[mask]),get_centroid(data[~mask])]
+
+def get_LDA_line(data,mask):
+    # return difference of two centroids - corresponds to a line
+    # note: should probably be normalized in practice
+    centroids = get_masked_centroids(data,mask)
+    return centroids[0] - centroids[1]
+
+def spam_score(encoded,line):
+    # score by projecting onto the line between the centroids.
+    # essentially - measure closeness to centroid ignoring orthogonal directions to the centroid difference
+    return encoded.dot(line)
+
+def sms_spam_score(encoded):
+    # apply spam_score to encoded using sms data
+    sms = preprocess()
+    data = encode(sms.text)
+    mask = sms.spam.astype(bool).values
+    line = get_LDA_line(data,mask)
+    return spam_score(encoded,line)
+
+def sms_self_spam_score():
+    # apply sms_spam_score to the dataset itself.
+    sms = preprocess()
+    data = encode(sms.text)
+    return sms_spam_score(data)
