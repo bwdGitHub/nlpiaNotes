2acfc2a66ac48f56334c1dadb17609d07d10208e
Ben Wells-Day <bwellsd@mathworks.com>
2020-04-19 14:44:22 +0100

    just a comment...book is slow here

---
+++ chapter4/LSA.py
@@ -0,0 +1,4 @@
+# the book rants on and on that LSA is SVD.
+# In particular - encode your corpus in a matrix W which is (vocab_size,corpus_size).
+# SVD this into p topics.
+# encode = bag_of_words, tfidf, whatever.
