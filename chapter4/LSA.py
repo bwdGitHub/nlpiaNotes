aa34f54a41217b9b25107729df58e2b58fecde26
Ben Wells-Day <bwellsd@mathworks.com>
2020-06-07 17:49:23 +0100

    meh

--- chapter4/LSA.py
+++ chapter4/LSA.py
@@ -5,7 +5,7 @@
 
 # W -> USV' 
 # U - left singular, (vocab_size,num_topics) -> U is the cross-correlation of words and topics.
-from nlpia.book.examples.ch04_catdog_lsa_sorted import lsa_models,prettify_tdm
+from nlpia.book.examples.ch04_catdog_lsa_sorted import lsa_models,prettify_tdm # note - have to debug this, usually adding encoding='utf8' in places.
 import numpy as np
 import pandas as pd
 
@@ -14,6 +14,15 @@ def catdog_svd():
     bow_svd = lsa_models()
     tdm = bow_svd[0]['tdm']
     U,S,Vt = np.linalg.svd(tdm)
+    # U is vocab_size x latent_dim (num_topics)
     U = pd.DataFrame(U,index = tdm.index).round(2)
-    return U
+    # S actually just returns the diagonal to save space.
+    # It can be reshaped to a diagonal latent_dim x latent_dim matrix, or a latent_dim x corpus_size matrix.
+    S = np.diag(S) # use fill_diagonal with a non-square zeros matrix to get latent_dim x corpus_size.
+    zeros = np.zeros((S.shape[0],Vt.shape[0]-S.shape[1]))
+    S = np.concatenate((S,zeros),axis=1)
+    # Trick: Make S into an identity to get rid of variance information
+    S[S>0] = 1
+
+    return (U,S,Vt)
 
