84c67f67badeb9e5a3430cebdb955173422afbf1
Ben Wells-Day <bwellsd@mathworks.com>
2020-06-14 22:43:01 +0100

    finishing off LSA/SVD/PCA examples

--- chapter4/PCA.py
+++ chapter4/PCA.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 pd.set_option('display.max_columns',6)
 from sklearn.decomposition import PCA
 import seaborn
@@ -36,10 +37,24 @@ def sms_pca(tfidf,sms,n_components = 16):
     topics = pd.DataFrame(topics,columns = cols, index = index)
     return (topics,pca)
 
-def get_topic_weights()
+def get_topic_weights():
     tfidf,sms,model = get_sms_data()
     topics,pca = sms_pca(tfidf,sms)
     col_nums,terms = zip(*sorted(zip(model.vocabulary_.values(),model.vocabulary_.keys())))
     w = pd.DataFrame(pca.components_,columns = terms, index = ['topic{}'.format(i) for i in range(16)])
     return w
 
+from sklearn.decomposition import TruncatedSVD
+svd = TruncatedSVD(n_components=16,n_iter = 100)
+tfidf,_,_ = get_sms_data()
+topics = svd.fit_transform(tfidf.values)
+# this is num_docs x num_topics - can't be bothered with the df.
+# of course this means downstream might get the wrong order...
+norm = np.expand_dims(np.linalg.norm(topics,axis=1),axis=1)
+norm_topics = topics/norm
+some = norm_topics[0:10,:]
+cosine_sim = some.dot(some.T)
+# the point here is that cosine_sim[i,j] is the similarity of document i to document j.
+# This simple example shows the spam messages as spam messages as being similar to each other
+# This is all in terms of the topic model - in fact we dot-producted over the topic dimension
+# An ML model can assign weights to those topics for the task at hand.
